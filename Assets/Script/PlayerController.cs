813d6253e7457f3a9afad6dc0eb450f8c7bb88ae
Batuhan <62680041+Abodat@users.noreply.github.com>
2024-05-06 06:02:55 +0300

    new game

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    public float moveSpeed = 3f;
+    public float jumpPower = 6f;
+
+    private bool isFacingRight = true;
+    
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform grounCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown("w") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+
+        if (Input.GetKeyUp("w") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            Debug.Log("mak");
+
+        }
+        
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(grounCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
